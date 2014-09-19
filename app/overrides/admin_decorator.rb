ec07976f321eabee9a3be0aad14811982bd2acf4
kannanhudson <kannanhudson@gmail.com>
2014-09-19 17:35:14 +0530

    Initial commit

---
+++ app/overrides/admin_decorator.rb
@@ -0,0 +1,5 @@
+
+Deface::Override.new(:virtual_path => "spree/admin/shared/_product_sub_menu",
+                     :name => "reviews_admin_tab",
+                     :insert_bottom => "[data-hook='admin_product_sub_tabs']",
+                     :text => "<%= tab(:latest_products, :label => 'Latest Products') %>")
